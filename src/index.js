e3d99fcbc6c65582529f7e322f40e324e1a4dbbb
YeduKrishnan A <kuttuzz17@gmail.com>
2023-01-04 13:48:21 +0530

    Main Page First Layout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 /* eslint-disable no-underscore-dangle */
-import _ from 'lodash'
+import _ from 'lodash';
+
+const item = document.querySelector('.item');
+const home = document.querySelector('.home');
 
 const comp = () => {
-    const element = document.createElement('div')
-    element.textContent = _.join(['Hello', 'Webpack'], ' ')
-    return element
+    const element = document.createElement('div');
+    element.textContent = _.join(['Demo', 'Webpack'], ' ');
+    return element;
 }
-document.body.append(comp())
+item.addEventListener("click", e => {
+    document.body.classList.add('newBody');
+    // alert('damn');
+})
+home.addEventListener("click", e => {
+    document.body.classList.remove('newBody');
+    // alert('damn');
+})
+// document.body.append(comp())
