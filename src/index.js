7515c6cf8381d6959b7d3c3daea02f644485a49d
YeduKrishnan A <kuttuzz17@gmail.com>
2023-01-05 14:41:01 +0530

    Create restaurant barebone structure using HTML, Css, .js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import _ from 'lodash';
 
 const item = document.querySelector('.item');
 const home = document.querySelector('.home');
+const contact = document.querySelector('.contact');
+const h1 = document.querySelector('h1');
+const foodItems = document.querySelector('.foodItems');
+
 
 const comp = () => {
     const element = document.createElement('div');
@@ -10,11 +14,29 @@ const comp = () => {
     return element;
 }
 item.addEventListener("click", e => {
-    document.body.classList.add('newBody');
+    document.body.classList.add('itemBody');
+    document.body.classList.remove('contactBody');
+    h1.style.display = "none";
+    h1.style.opacity = 0;
+    foodItems.style.opacity = 100;
     // alert('damn');
 })
 home.addEventListener("click", e => {
-    document.body.classList.remove('newBody');
+    document.body.classList.add('homeBody');
+    document.body.classList.remove('itemBody');
+    document.body.classList.remove('contactBody');
+    h1.style.display = "block";
+    h1.style.opacity = 100;
+    foodItems.style.opacity = 0;
+
+    // alert('damn');
+})
+contact.addEventListener("click", e => {
+    document.body.classList.remove('itemBody');
+    document.body.classList.add('contactBody');
+    h1.style.opacity = 0;
+    foodItems.style.opacity = 0;
+
     // alert('damn');
 })
 // document.body.append(comp())
